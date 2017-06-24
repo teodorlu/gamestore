21f45b6dc5adf8693a36f6e9309bb8549dd1633f
Teodor Heggelund <teodor.heggelund@gmail.com>
2017-06-24 12:38:37 +0200

    Add flow and containers

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,17 @@ module Lib
     ( someFunc
     ) where
 
+import Data.Map as Map
+import Data.Maybe as Maybe
+import Flow ((|>))
+
+getname name =
+  Map.lookup name |> Maybe.maybe
+
+hello :: String -> String
+hello name = "Hello " ++ name ++ "!"
+
 someFunc :: IO ()
-someFunc = putStrLn "Hello, World!"
+someFunc = do
+  putStrLn $ hello "Teodor"
+  print $ Map.fromList [("1", "Num"), ("2", "Num"), ("c", "Char")]
